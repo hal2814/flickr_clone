debc8b00cc4cf6ddee80dd1fa68fdf31366f9286
Gabrijela Gavric, Nate McGregor and Qudsia Ahmad <qudsia@>
2017-11-29 10:44:12 -0800

    add validations and relationships

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,11 @@
 class Photo < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+  has_many :tags
+  validates :image, :presence => true
+  validates :view_count, :presence => true
+  validates :description, :presence => true
+  validates :location, :presence => true
+  validates :date, :presence => true
+  validates :camera_type, :presence => true
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ApplicationRecord
+  has_many :photos
+  belongs_to :photo
+  validates :tag, :presence => true
 end
