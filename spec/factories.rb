94f4bf81b2b5fcaa195ae8244fa758a179eb5805
Gabrijela Gavric, Nate McGregor and Qudsia Ahmad <nate@>
2017-11-29 16:40:09 -0800

    add new integration tests

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory(:user) do
+    username('monkey')
+    email('dan@dandan.com')
+    password('123456')
+  end
+end
---
+++ spec/features/login_spec.rb
@@ -0,0 +1,14 @@
+# require 'rails_helper'
+#
+# describe "the login process" do
+#   user = FactoryBot.create(:user)
+#   it "logs a user in" do
+#     visit photos_path
+#     click_link 'Login'
+#     fill_in 'Email', :with => user.email
+#     fill_in 'Password', :with => user.password
+#
+#     click_on 'Log in'
+#     expect(page).to have_content 'Signed in successfully.'
+#   end
+# end
